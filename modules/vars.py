d2228ea4a7f4ca980b6bdae2c42e5bc7527eadbe
legendofficial12 <skphotograph029@gmail.com>
2024-06-23 15:40:25 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "29224205"
 api_hash = "43e998f17bcf1f69f85b6d4022dffde2"
-bot_token = "7395837796:AAHv4CU7iYQPf1C6KE0VYzsFd_ZXD02LYKg"
+bot_token = "7349142517:AAEV3pkUy18WoJ-jThxkAU5v3EcsLutJ-kw"
