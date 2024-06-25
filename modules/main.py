a80d6b5aa1be9e60b632bca33fcab6eb29d39639
legendofficial12 <skphotograph029@gmail.com>
2024-06-25 07:17:12 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -162,8 +162,11 @@ async def account_login(bot: Client, m: Message):
 
             try:  
                 
-                cc = f'**[📽️] Vid_ID:** {str(count).zfill(3)}.** {𝗻𝗮𝗺𝗲𝟭}{MR}.mkv\n**  𝔹ᴀᴛᴄʜ** » **  {raw_text0}**'
-                cc1 = f'**[📁] Pdf_ID:** {str(count).zfill(3)}. {𝗻𝗮𝗺𝗲𝟭}{MR}.pdf \n**  𝔹ᴀᴛᴄʜ** » **  {raw_text0}**'
+                cc = f'**[📽️] Vid_ID:** {str(count).zfill(3)}.** {𝗻𝗮𝗺𝗲𝟭}.mkv\n** 
+                𝔹ᴀᴛᴄʜ** » **  {raw_text0}**'
+                {MR}
+                cc1 = f'**[📁] Pdf_ID:** {str(count).zfill(3)}. {𝗻𝗮𝗺𝗲𝟭}{MR}.pdf \n**  
+                𝔹ᴀᴛᴄʜ** » **  {raw_text0}**'
                 if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
