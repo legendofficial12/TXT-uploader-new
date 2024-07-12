dfdd33a6e046700977ccac25482283abdba0af90
legendofficial12 <skphotograph029@gmail.com>
2024-07-12 09:31:17 +0530

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -66,7 +66,6 @@ def hrt(seconds, precision = 0):
 
 timer = Timer()
 
-# Powered By Ankush
 async def progress_bar(current, total, reply, start):
     if timer.can_send():
         now = time.time()
@@ -89,10 +88,10 @@ async def progress_bar(current, total, reply, start):
             bar_length = 11
             completed_length = int(current * bar_length / total)
             remaining_length = bar_length - completed_length
-            progress_bar = "▰" * completed_length + "▱" * remaining_length
+            progress_bar = "▓" * completed_length + "▒" * remaining_length
             
             try:
-                await reply.edit(f'\n `╭──⌯════🆄︎ᴘʟᴏᴀᴅɪɴɢ⬆️⬆️═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n├🤖𝔹ʏ »𝗟𝗲𝗴𝗲𝗻𝗱 𝗢𝗳𝗳𝗶𝗰𝗶𝗮𝗹🦅\n╰─═══ ✪ @Hacker_Ks ✪ ═══─╯\n`') 
+                await reply.edit(f'`\n ╭──⌯════Bot Statics═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n╰─═══ ༺ 𝗟𝗲𝗴𝗲𝗻𝗱 𝗢𝗳𝗳𝗶𝗰𝗶𝗮𝗹 🦅༻ ═══─╯`\n') 
             except FloodWait as e:
                 time.sleep(e.x)
-
+                
