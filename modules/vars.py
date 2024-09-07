7b3971c1eac5b15f4517dfff180cfd507866151c
legendofficial12 <skphotograph029@gmail.com>
2024-09-07 11:28:46 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "29500466"
 api_hash = "97de63084b44bb7ca5ab11d1252fa098"
-bot_token = "6669593990:AAG4l-0bVtjGx7hNH34mDV9iUqEIlYM5hhE"
+bot_token = "7128227567:AAEHNput6QVLo4W56K-7L-io5SyG4LxyIq0"
